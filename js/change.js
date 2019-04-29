6d6d4540823db8317b471288c922f6700dcee7be
AidanSpeaks <aidanrjob@gmail.com>
2019-04-29 11:05:01 -0500

    start test
    afafiafififaskm sams cool

---
+++ js/change.js
@@ -0,0 +1,4 @@
+var 2 = document.cookie = 'game2=' + game2input + '"';
+var 4 = document.cookie = 'game4=' + game4input + '"';
+addStyleString('  .tile.tile-2 .tile-inner { background-image: url("' + 2 + '"}');
+addStyleString('  .tile.tile-4 .tile-inner { background-image: url("' + 4 + '"}');
