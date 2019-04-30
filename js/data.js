b26d2949011a7bfc68e801b51745fc58524b127c
AidanSpeaks <aidanrjob@gmail.com>
2019-04-30 11:38:10 -0500

    commented out until i can fix it
    mk ;)??? i dont know what to type here anymore.....

--- js/data.js
+++ js/data.js
@@ -1,12 +1,12 @@
-var urls = document.cookie()
+//var urls = document.cookie()
 
-var encrypted = CryptoJS.AES.encrypt(urls, "Secret Passphrase");
+//var encrypted = CryptoJS.AES.encrypt(urls, "Secret Passphrase");
 //U2FsdGVkX18ZUVvShFSES21qHsQEqZXMxQ9zgHy+bu0=
 
-var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+//var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
 //4d657373616765
 
 
-document.getElementById("demo1").innerHTML = encrypted;
-document.getElementById("demo2").innerHTML = decrypted;
-document.getElementById("demo3").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
+//document.getElementById("demo1").innerHTML = encrypted;
+//document.getElementById("demo2").innerHTML = decrypted;
+//document.getElementById("demo3").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
