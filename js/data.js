f946c2d52275632b9f9535b3d88aab5b1eb81e00
AidanSpeaks <aidanrjob@gmail.com>
2019-05-03 13:54:29 -0500

    more sign in js

--- js/data.js
+++ js/data.js
@@ -4,3 +4,29 @@ firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error
   var errorMessage = error.message;
   // ...
 });
+
+function field_focus(field, email)
+  {
+    if(field.value == email)
+    {
+      field.value = '';
+    }
+  }
+
+  function field_blur(field, email)
+  {
+    if(field.value == '')
+    {
+      field.value = email;
+    }
+  }
+
+//Fade in dashboard box
+$(document).ready(function(){
+    $('.box').hide().fadeIn(1000);
+    });
+
+//Stop click event
+$('a').click(function(event){
+    event.preventDefault(); 
+	});
