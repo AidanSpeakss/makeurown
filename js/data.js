245136aede9af4da57982211e59e134e4f63802a
AidanSpeaks <aidanrjob@gmail.com>
2019-05-03 13:50:37 -0500

    added firebase to this doc

--- js/data.js
+++ js/data.js
@@ -1,12 +1,6 @@
-//var urls = document.cookie()
-
-//var encrypted = CryptoJS.AES.encrypt(urls, "Secret Passphrase");
-//U2FsdGVkX18ZUVvShFSES21qHsQEqZXMxQ9zgHy+bu0=
-
-//var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
-//4d657373616765
-
-
-//document.getElementById("demo1").innerHTML = encrypted;
-//document.getElementById("demo2").innerHTML = decrypted;
-//document.getElementById("demo3").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
