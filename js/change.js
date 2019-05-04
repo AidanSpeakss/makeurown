83c0c606c8ebc0304de7c3bcc0547dfe71071034
AidanSpeaks <aidanrjob@gmail.com>
2019-05-04 14:53:57 -0500

    update2

--- js/change.js
+++ js/change.js
@@ -44,10 +44,9 @@ addStyleString(' #loader {display: inline-block; } ');
         var showing = true;
     });
     if(showing = true) {
-document.body.getElementById("authbehind")[0].addEventListener("click", function closeSignWindow() { 
 addStyleString(' #firebaseui-auth-container { display: none;}');
 addStyleString(' #loader {display: none; }');
-})};
+};
 };
 injectCSS();
 document.body.getElementsByClassName("go")[0].addEventListener("click", function cookkies() { 
