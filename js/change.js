a68f13f5cf8cf4d8ad5d66334847487b976ab03c
AidanSpeaks <aidanrjob@gmail.com>
2019-05-20 09:28:34 -0500

    fixed equal too

--- js/change.js
+++ js/change.js
@@ -673,7 +673,7 @@ function saveCloud() {
 function loadSaves() {
     db.collection("users").doc(firebase.auth().currentUser.uid).get().then(function(doc) {
         if (!doc.data().gameNumber > 11) {
-            while (doc.data().gameNumber > 1, gameNumberAdd--) {
+            while (doc.data().gameNumber > 1 || doc.data().gameNumber = 1, gameNumberAdd--) {
                 var add = document.createElement("p");
                 add.innerHTML = "Saved Game #" + gameNumberAdd;
                 add.className = "game-button " + "game" + gameNumberAdd;
